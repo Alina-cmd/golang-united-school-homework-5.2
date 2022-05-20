9180653e63cfed73009624667ddf6584c1fc3f04
Alina <you@example.com>
2022-05-20 14:31:26 +0300

    hw_5.2

--- cache.go
+++ cache.go
@@ -2,22 +2,53 @@ package cache
 
 import "time"
 
+type valueDeadline struct {
+	value     string
+	deadline  time.Time
+	unlimited bool
+}
+
 type Cache struct {
+	m map[string]valueDeadline
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	cache, ok := c.m[key]
+	if !ok {
+		return "", ok
+	}
 
+	if cache.unlimited {
+		return cache.value, true
+	} else {
+		if cache.deadline.Before(time.Now()) {
+			return cache.value, true
+		}
+	}
+
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.m[key] = valueDeadline{value, time.Now(), true}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0, len(c.m))
+	for key, value := range c.m {
+		if value.unlimited {
+			keys = append(keys, key)
+		} else if value.deadline.Before(time.Now()) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.m[key] = valueDeadline{value, deadline, false}
 }
